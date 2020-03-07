8b37b2eebcf41e53ecb5ca056dfebb55511f7ec8
Y-Dan Bui <ydanbui@gmail.com>
2020-03-07 13:06:30 -0800

    header badge functionality + basic edit module

--- todo-app.js
+++ todo-app.js
@@ -1,6 +1,10 @@
 const searchBar = document.querySelector('.header__search')
 const addTaskBtn = document.querySelector('.btn--add-task')
 const todoSection = document.querySelector('.todo')
+const editModule = document.querySelector('.edit')
+const headerBadgeAll = document.querySelector('#headerBadgeAll') 
+const headerBadgeInProgress = document.querySelector('#headerBadgeInProgress') 
+const headerBadgeCompleted = document.querySelector('#headerBadgeCompleted') 
 
 const todos = [{
     text: 'You have no tasks! Add one!',
@@ -14,7 +18,7 @@ const todos = [{
     dueDate: 'Feb 28'
 }, {
     text: 'You have aid!',
-    completed: false,
+    completed: true,
     label: [],
     dueDate: 'Feb 28'
 }]
@@ -24,16 +28,34 @@ const filters = {
     labelTag: null
 }
 
-// Render todos cards in .todo section
+// Function to render badges in header
+const renderBadges = todos => {
+    const total = todos.length
+    let inProgress = 0
+    let completed = 0
+
+    // Count number of todos that are in progress and completed
+    todos.forEach(todo => {
+        todo.completed ? completed++ : inProgress++
+    })
+
+    // Set badges text content
+    headerBadgeAll.textContent = total
+    headerBadgeInProgress.textContent = inProgress
+    headerBadgeCompleted.textContent = completed
+}
+
+// Function to render todos cards in .todo section
 const renderTodos = (todos, filters) => {
+    // New array of filtered todos
     const filteredTodos = todos.filter(todo => {
         return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
     })
 
-    // Clear existing todos before rendering
+    // Clear existing todo elements before rendering
     todoSection.innerHTML = ''
 
-    // Print each todo text
+    // Print each todo in the filtered todo array
     filteredTodos.forEach(todo => {
         const todoCard = document.createElement('article')
         todoCard.textContent = todo.text
@@ -42,15 +64,34 @@ const renderTodos = (todos, filters) => {
 }
 
 renderTodos(todos, filters)
+renderBadges(todos)
 
-// Search bar event handler
+// Search bar input event handler
 searchBar.addEventListener('input', e => {
     console.log(e.target.value)
+    // Set filter to be search input
     filters.searchText = e.target.value
+    // Render todos with new filter
     renderTodos(todos, filters)
 })
 
-// Add new task buttom
+// Add new task button click event handler
 addTaskBtn.addEventListener('click', e => {
     console.log('add new task')
+
+    // Add new todo to end of todo array
+    todos.push({
+        text:'Write task',
+        completed: false,
+        label: [],
+        dueDate: null
+    })
+
+    // Rerender todos and badge
+    renderTodos(todos, filters)
+    renderBadges(todos)
+
+    // Display edit module
+    editModule.style.display = "block"
+
 })
