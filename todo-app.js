d4a4f930d1ed82d1d906fe04773db4306ef8f65d
Y-Dan Bui <ydanbui@gmail.com>
2020-03-06 19:46:25 -0800

    add basic todo rendering

--- todo-app.js
+++ todo-app.js
@@ -1,19 +1,55 @@
+const searchBar = document.querySelector('.header__search')
+const addTaskBtn = document.querySelector('.btn--add-task')
+const todoSection = document.querySelector('.todo')
+
 const todos = [{
     text: 'You have no tasks! Add one!',
     completed: false,
     label: [],
     dueDate: 'Feb 28'
+}, {
+    text: 'Second task!',
+    completed: false,
+    label: [],
+    dueDate: 'Feb 28'
+}, {
+    text: 'You have aid!',
+    completed: false,
+    label: [],
+    dueDate: 'Feb 28'
 }]
 
-// Print each todo text
-todos.forEach(todo => {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('.todo').appendChild(p)
+const filters = {
+    searchText: ''
+}
+
+// Render todos cards in .todo section
+const renderTodos = (todos, filters) => {
+    const filteredTodos = todos.filter(todo => {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    // Clear existing todos before rendering
+    todoSection.innerHTML = ''
+
+    // Print each todo text
+    filteredTodos.forEach(todo => {
+        const todoCard = document.createElement('article')
+        todoCard.textContent = todo.text
+        todoSection.appendChild(todoCard)
+    })
+}
+
+renderTodos(todos, filters)
+
+// Search bar event handler
+searchBar.addEventListener('input', e => {
+    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
 
 // Add new task buttom
-const addTaskBtn = document.querySelector('.btn--add-task')
 addTaskBtn.addEventListener('click', e => {
     console.log('add new task')
 })
